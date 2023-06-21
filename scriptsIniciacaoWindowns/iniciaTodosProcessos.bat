b41b417fff3e5986f76d5a7491d4c921610fb12d
willian <mynamewra@gmail.com>
2023-06-20 21:29:16 -0300

    Add validation and script bat windowns

---
+++ scriptsIniciacaoWindowns/iniciaTodosProcessos.bat
@@ -0,0 +1,9 @@
+start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 
+
+start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor2 4 3004 
+
+start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor1 3 3003 
+
+start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente  gerente2 2 3002 
+
+start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente gerente1 1 3001 
