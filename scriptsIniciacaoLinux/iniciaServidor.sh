4a1cdcf02077e2a47442f237013cb94ef0c648ef
willian <mynamewra@gmail.com>
2023-06-21 20:29:32 -0300

    fix scripts

--- scriptsIniciacaoLinux/iniciaServidor.sh
+++ scriptsIniciacaoLinux/iniciaServidor.sh
@@ -1,1 +1,1 @@
-gnome-terminal -- java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 
+cd .. && gnome-terminal -- java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 
