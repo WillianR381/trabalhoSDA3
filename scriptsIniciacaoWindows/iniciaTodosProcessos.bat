6f2d9c35b4b6164c88698aa7e94896834d1146ec
willian <mynamewra@gmail.com>
2023-06-21 20:32:02 -0300

    fix scripts

--- scriptsIniciacaoWindows/iniciaTodosProcessos.bat
+++ scriptsIniciacaoWindows/iniciaTodosProcessos.bat
@@ -1,9 +1,9 @@
-start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 
+cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 ; cd scriptsIniciacaoWindows
 
-start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor2 4 3004 
+cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor2 4 3004 ; cd scriptsIniciacaoWindows
 
-start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor1 3 3003 
+cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor1 3 3003 ; cd scriptsIniciacaoWindows
 
-start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente  gerente2 2 3002 
+cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente  gerente2 2 3002 ; cd scriptsIniciacaoWindows
 
-start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente gerente1 1 3001 
+cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente gerente1 1 3001 ; cd scriptsIniciacaoWindows
