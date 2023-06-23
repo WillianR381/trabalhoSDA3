ffff8433713ae55d4423749ade83f4dbb714e3c4
WillianR381 <52840407+WillianR381@users.noreply.github.com>
2023-06-23 02:11:41 -0300

    Delete iniciaTodosProcessos.bat

--- scriptsIniciacaoWindows/iniciaTodosProcessos.bat
+++
@@ -1,9 +0,0 @@
-cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar servidor servidor 5 3005 ; cd scriptsIniciacaoWindows
-
-cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor2 4 3004 ; cd scriptsIniciacaoWindows
-
-cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar vendedor vendedor1 3 3003 ; cd scriptsIniciacaoWindows
-
-cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente  gerente2 2 3002 ; cd scriptsIniciacaoWindows
-
-cd .. ; start java -jar target/trabalhoSDA3-1.0-SNAPSHOT.jar gerente gerente1 1 3001 ; cd scriptsIniciacaoWindows
